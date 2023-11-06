ce125818c70495b3e26cf405843cfc0c932e19cd
Jay <138824448+jaythemself@users.noreply.github.com>
2023-11-05 20:34:25 -0600

    task 4 finished with passing tests

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -41,7 +41,7 @@ public class SearchController {
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
 
-        if (searchTerm.isEmpty() || searchTerm.equalsIgnoreCase("all")) {
+        if (searchTerm.isBlank() || searchTerm.equalsIgnoreCase("all")) {
             jobs = JobData.findAll();
         } else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
